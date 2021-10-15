d1d1464cd124211644c9769c92c019590cad71ec
Henadzi <68184678+oreshkevich@users.noreply.github.com>
2021-10-15 09:25:08 +0300

    Update binary-search-tree.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,39 +8,120 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 module.exports = class BinarySearchTree {
 
+ constructor() {
+    this.rootOne = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootOne;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootOne = host(this.rootOne, data);
+    function host(node, specs) {
+      if (node === null) {
+        return new Node(specs);
+      }
+       if (specs < node.data) {
+         node.left = host(node.left, specs);
+      } else {
+       
+        node.right = host(node.right, specs);
+      }
+      if (specs === node.data) {
+        return specs;
+      }
+     
+      return node;
+    }
+  }
+  has(data ) {
+   let vertex = this.rootOne;
+   while(vertex) {
+     if (data == vertex.data) return true;
+     if(data > vertex.data) {
+       vertex = vertex.right;
+     } else {
+       vertex = vertex.left;
+     }
+   }
+   return false;
+  }
+ 
+ find(data ) {
+       let vertex = this.rootOne;
+
+    while (vertex.data !== data) {
+      vertex = (data < vertex.data) ? vertex.left : vertex.right;
+        
+      if (vertex === null) return null; 
+    }
+
+    return vertex;
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  remove(data) {
+    this.rootOne = removeOne(this.rootOne, data);
+
+    function removeOne(node, specs) {
+      if(node === null) 
+      return null;
+      if(specs === node.data) {
+        if(!node.left && !node.right) return null;
+      
+        if(node.left === null) {
+          node = node.right;
+          return node;
+        }
+        if(node.right === null) {
+          node =node.left;
+          return node;
+        }
+
+        let minRight = node.right;
+        while(minRight.left) {
+          minRight = minRight.left;
+        }
+        node.data = minRight.data;
+        node.right = removeOne(node.right, minRight.data)
+
+        return node;
+      } else if(specs < node.data) {
+        node.left = removeOne(node.left, specs);
+        return node;
+      } else if(specs > node.data) {
+        node.right = removeOne(node.right, specs);
+        return node; 
+      } 
+    }
+    
+    
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ 
+min() {
+    let vertex = this.rootOne;
+    let minOne = vertex.data;
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    while(vertex){
+      minOne = vertex.data;
+      vertex = vertex.left;
+    }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return minOne;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let vertex = this.rootOne;
+    let maxOne = vertex.data;
+
+    while(vertex){
+      maxOne = vertex.data;
+      vertex = vertex.right;
+    }
+
+    return maxOne;
   }
 
 }
