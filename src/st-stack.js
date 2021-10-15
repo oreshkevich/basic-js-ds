a8f1c00a34d1626fd8e12568cd54c2b8a41f3252
Henadzi <68184678+oreshkevich@users.noreply.github.com>
2021-10-15 15:05:49 +0300

    Update st-stack.js

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+     this.answer = []
+   }
+  push( element ) {
+   this.answer.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.answer.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.answer[this.answer.length - 1 ]
   }
 }
